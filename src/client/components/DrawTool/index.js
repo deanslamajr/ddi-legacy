643c8cd6f3d9deddd5c502d4596808265420351f
Dean Slama Jr <deanslamajr@gmail.com>
2016-11-24 15:01:37 -0600

    added mouse & touch events

--- src/client/components/DrawTool/index.js
+++ src/client/components/DrawTool/index.js
@@ -1,6 +1,57 @@
 import React from 'react';
 
 export default class DrawTool extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.ctx = null;
+
+    this.onDown = this.onDown.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onUp = this.onUp.bind(this);
+
+    this.state = {
+      active: false,
+      x: 0,
+      y: 0
+    };
+  }
+
+  onDown(event) {
+    // handle the first touch event or the mouse click
+    const evt = event.changedTouches && event.changedTouches[0] || event;
+
+    this.setState({
+      active: true,
+      x: evt.pageX,
+      y: evt.pageY
+    });
+  }
+
+  onMove(event) {
+    event.preventDefault();
+    // handle the first touch event or the mouse click
+    const evt = event.changedTouches && event.changedTouches[0] || event;
+
+    if (this.state.active) {
+      this.setState({
+        x: evt.clientX,
+        y: evt.clientY
+      });
+    }
+  }
+
+  onUp(event) {
+    // handle the first touch event or the mouse click
+    const evt = event.changedTouches && event.changedTouches[0] || event;
+
+    this.setState({
+      active: false,
+      x: evt.pageX,
+      y: evt.pageY
+    })
+  }
+
   componentDidMount() {
     this.updateCanvas();
   }
@@ -8,16 +59,31 @@ export default class DrawTool extends React.Component {
   updateCanvas() {
     const { width, height } = this.props;
 
-    const ctx = this.refs.canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0,0, width, height);
+    this.ctx = this.refs.canvas.getContext('2d');
+    this.ctx.fillStyle = 'white';
+    this.ctx.fillRect(0,0, width, height);
   }
   
   render() {
     const { width, height } = this.props;
 
     return (
-      <canvas ref="canvas" width={width} height={height}/>
+      <div>
+        <canvas 
+          ref='canvas' 
+          id='canvas' 
+          width={width} 
+          height={height}
+          onMouseDown={this.onDown}
+          onTouchStart={this.onDown}
+          onMouseMove={this.onMove}
+          onTouchMove={this.onMove}
+          onMouseUp={this.onUp}
+          onTouchEnd={this.onUp}
+          />
+        <div>{this.state.x}</div>
+        <div>{this.state.y}</div>
+      </div>
     );
   }
 }

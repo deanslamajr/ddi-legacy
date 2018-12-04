df707545cef7fb77f3a4d884db261c2aff389c24
Dean Slama <deanslamajr@gmail.com>
2018-12-03 17:17:52 -0700

    wip. got canvas downloading working

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,30 @@ import { Component } from 'react'
 import styled from 'styled-components'
 import Konva from 'konva'
 import { Stage, Layer, Image, Rect } from 'react-konva'
-import { Emoji, Picker } from 'emoji-mart'
+import { Emoji, Picker, emojiIndex } from 'emoji-mart'
 import domtoimage from 'dom-to-image'
 
 import 'emoji-mart/css/emoji-mart.css'
 
-const CanvasContainer = styled.div`
+// console.log('emojiIndex.emojis')
+// Object.values(emojiIndex.emojis).filter(emoji => emoji.native).map(emoji => console.dir(emoji.native))
+// console.dir(emojiIndex)
+
+// function from https://stackoverflow.com/a/15832662/512042
+function downloadURI(uri, name) {
+  const link = document.createElement("a");
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+const CenteredContainer = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `
 
 class Test extends Component {
@@ -37,6 +53,23 @@ class Test extends Component {
             console.error('oops, something went wrong!', error);
         })
     })
+  }
+
+  saveCell = (event) => {
+    console.log('this.stage')
+    const imageUri = this.stage.getStage().toDataURL()
+    downloadURI(imageUri, 'tester')
+  }
+
+  searchEmojis = (event) => {
+    console.log('event.target.value')
+    console.dir(event.target.value)
+    const results = emojiIndex.search(event.target.value)
+    if (results) {
+      results.filter(emoji => emoji.native).map(emoji => {
+        console.dir(emoji.native)
+      })
+    }
   }
 
   render () {
@@ -68,8 +101,8 @@ class Test extends Component {
           {/* Images for this Card support an aspect ratio of 1:1 with minimum dimensions of 144x144 or maximum of 4096x4096 pixels. Images must be less than 5MB in size. The image will be cropped to a square on all platforms. JPG, PNG, WEBP and GIF formats are supported. Only the first frame of an animated GIF will be used. SVG is not supported  */}
           <meta name="twitter:image" content={`https://s3-us-west-2.amazonaws.com/assets.dslama.net/test${v}.jpg`} />
         </Head>
-        <CanvasContainer>
-          <Stage width={250} height={250}>
+        <CenteredContainer>
+          <Stage ref={ref => this.stage = ref} width={250} height={250}>
             <Layer>
               <Rect
                 x={0}
@@ -81,14 +114,16 @@ class Test extends Component {
               <Image image={this.state.image} scale={{ x: 2, y: 2 }} />
             </Layer>
           </Stage>
-        </CanvasContainer>
-        <Picker onSelect={this.selectEmoji} />
-        {this.state.selectedEmoji && <div id='taco'>
-          <Emoji
-            emoji={this.state.selectedEmoji}
-            size={64}
-          />
-        </div>}
+          <input type="button" onClick={this.saveCell} value='Save!' />
+          <Picker onSelect={this.selectEmoji} />
+          {this.state.selectedEmoji && <div id='taco'>
+            <Emoji
+              emoji={this.state.selectedEmoji}
+              size={64}
+            />
+          </div>}
+          <input onChange={this.searchEmojis} type='text' />
+        </CenteredContainer>
       </div>
     )
   }

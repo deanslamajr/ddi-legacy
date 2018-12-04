65b00d9c663b94cddb53b9e07e72f9fc3da39cb8
Dean Slama <deanslamajr@gmail.com>
2018-12-04 06:54:53 -0700

    wip. added server page index.js, added nodemon

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const next = require('next')
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare()
+  .then(() => {
+    const server = express()
+
+    // create a new cell - save image to s3 and save metadata to redis?
+    server.post('/cell', (req, res) => {
+      return res.status(200).json({ data: 'dis endpoint is valid!' })
+    })
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })

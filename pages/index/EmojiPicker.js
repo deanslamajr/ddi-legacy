b03230bdd5311df83768e6f72ab8e2503d995a80
Dean Slama <deanslamajr@gmail.com>
2019-01-11 08:05:23 -0700

    Began work on custom emojiPicker component

---
+++ pages/index/EmojiPicker.js
@@ -0,0 +1,74 @@
+import { Component } from 'react'
+import { emojiIndex } from 'emoji-mart'
+import styled from 'styled-components'
+
+const initialEmojiSet = Object.values(emojiIndex.emojis).filter(emoji => emoji.native).map(o => o.native)
+
+const OuterContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const SearchInput = styled.input`
+  width: 70%;
+`
+
+const EmojisContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 2.5rem;
+
+`
+
+const EmojiContainer = styled.span`
+  margin: 1rem;
+`
+
+const Emoji = ({ emoji, onSelect }) => {
+  return (
+    <EmojiContainer onClick={() => onSelect(emoji)} >
+      {emoji}
+    </EmojiContainer>
+  )
+}
+
+class EmojiPicker extends Component {
+  state = {
+    emojis: initialEmojiSet
+  }
+
+  handleChange = (event) => {
+    const searchValue = event.target.value
+    const emojis = searchValue !== ''
+      ? emojiIndex.search(event.target.value).sort(o => o.name).map((o) => o.native)
+      : initialEmojiSet
+
+    this.setState({ emojis })
+  }
+
+  render () {
+    return (
+      <OuterContainer>
+        <SearchInput type='text' name='search' onChange={this.handleChange} />
+        <EmojisContainer>
+          {this.state.emojis.map(emoji => (
+            <Emoji
+              key={emoji}
+              emoji={emoji}
+              onSelect={this.props.onSelect}
+            />))}
+        </EmojisContainer>
+      </OuterContainer>
+    )
+  }
+}
+
+export default EmojiPicker
---
+++ pages/index/index.js
@@ -0,0 +1,126 @@
+import Head from 'next/head'
+import { Router } from '../../routes'
+import { Component } from 'react'
+import styled from 'styled-components'
+import Konva from 'konva'
+import { Stage, Layer, Rect, Text } from 'react-konva'
+import axios from 'axios'
+import getConfig from 'next/config'
+import shortid from 'shortid'
+
+import EmojiPicker from './EmojiPicker'
+
+// Environment variables
+// @see {@link https://nextjs.org/docs/#exposing-configuration-to-the-server--client-side}
+const { publicRuntimeConfig } = getConfig()
+
+function generateFilename () {
+  return `${shortid.generate()}.png`
+}
+
+const CenteredContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class Test extends Component {
+  state = {
+    selectedEmoji: undefined,
+    showEmojiPicker: true,
+    showSaveButton: true
+  }
+
+  onEmojiSelect = (emoji) => {
+    console.log('selected emoji: %o', emoji)
+    this.setState({
+      chosenEmoji: emoji,
+      showEmojiPicker: false
+    })
+  }
+
+  getSignedRequest = async (file) => {
+    try {
+      const { data } = await axios.get(`/sign?file-name=${file.name}&file-type=${file.type}`)
+      return data
+    }
+    catch (e) {
+      throw e
+    }
+  }
+
+  saveCell = async (event) => {
+    this.setState({ showSaveButton: false })
+
+    this.stage.toCanvas().toBlob(async (blob) => {
+      const file = new File([blob], generateFilename(), {
+        type: 'image/png',
+      })
+
+      try {
+        const {
+          id,
+          signedRequest } = await this.getSignedRequest(file)
+
+        const xhr = new XMLHttpRequest()
+        xhr.open('PUT', signedRequest)
+        xhr.onreadystatechange = () => {
+          if(xhr.readyState === 4){
+            if(xhr.status === 200){
+              Router.pushRoute(`/i/${id}`)
+            }
+            else{
+              console.error('could not upload file!')
+            }
+          }
+        }
+        xhr.send(file)
+      }
+      catch (e) {
+        console.error(e)
+      }
+    })
+  }
+
+  openEmojiPicker = () => {
+    this.setState({ showEmojiPicker: true })
+  }
+
+  render () {
+    return (
+      <div>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <CenteredContainer>
+          <Stage ref={ref => this.stage = ref} width={250} height={250}>
+            <Layer>
+              <Rect
+                x={0}
+                y={0}
+                width={250}
+                height={250}
+                stroke={'black'}
+              />
+              {this.state.chosenEmoji && (
+                <Text
+                  text={this.state.chosenEmoji}
+                  fontSize={100}
+                />
+              )}
+            </Layer>
+          </Stage>
+
+          {this.state.showSaveButton && <input type="button" onClick={this.saveCell} value='Save!' />}
+
+          <input type="button" onClick={this.openEmojiPicker} value={this.state.chosenEmoji} />
+              
+          {this.state.showEmojiPicker && <EmojiPicker onSelect={this.onEmojiSelect} />}
+        </CenteredContainer>
+      </div>
+    )
+  }
+}
+
+export default Test 

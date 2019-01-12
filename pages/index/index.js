9c18fc2e7fe686e0c592e898dc407119303bf4f5
Dean Slama <deanslamajr@gmail.com>
2019-01-12 12:21:47 -0700

    Added more manipulation modes

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,6 +10,8 @@ import shortid from 'shortid'
 
 import EmojiPicker from './EmojiPicker'
 
+const konvaCacheConfig = { offset: 10 }
+
 // Environment variables
 // @see {@link https://nextjs.org/docs/#exposing-configuration-to-the-server--client-side}
 const { publicRuntimeConfig } = getConfig()
@@ -34,18 +36,26 @@ class Test extends Component {
     showEmojiPicker: true,
     showSaveButton: true,
     // Emoji
+    filters: undefined,
     selectedEmoji: undefined,
-    x: 0,
-    y: 0,
-    scale: 1,
-    size: 100
+    x: 100,
+    y: 100,
+    scaleX: 1,
+    scaleY: 1,
+    rotation: 0,
+    size: 100,
+    alpha: .5,
+    red: 125,
+    green: 0,
+    blue: 0
   }
 
   onEmojiSelect = (emoji) => {
-    console.log('selected emoji: %o', emoji)
     this.setState({
       chosenEmoji: emoji,
       showEmojiPicker: false
+    }, () => {
+      this.emoji.cache(konvaCacheConfig)
     })
   }
 
@@ -97,27 +107,79 @@ class Test extends Component {
   }
 
   moveUp = () => {
-    this.setState({ y: this.state.y - 10})
+    this.setState({ y: this.state.y - 10}, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   moveLeft = () => {
-    this.setState({ x: this.state.x - 10})
+    this.setState({ x: this.state.x - 10}, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   moveRight = () => {
-    this.setState({ x: this.state.x + 10})
+    this.setState({ x: this.state.x + 10}, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   moveDown = () => {
-    this.setState({ y: this.state.y + 10})
+    this.setState({ y: this.state.y + 10}, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   makeLarger = () => {
-    this.setState({ size: this.state.size + 1 })
+    this.setState({ size: this.state.size + 1 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   makeSmaller = () => {
-    this.setState({ size: this.state.size - 1 })
+    this.setState({ size: this.state.size - 1 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  anticlockwiseRotation = () => {
+    this.setState({ rotation: this.state.rotation - 10 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  clockwiseRotation = () => {
+    this.setState({ rotation: this.state.rotation + 10 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  flipX = () => {
+    this.setState({ scaleX: this.state.scaleX * -1 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  flipY = () => {
+    this.setState({ scaleY: this.state.scaleY * -1 }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  toggleFilter = () => {
+    const filters = this.state.filters
+      ? undefined
+      : [Konva.Filters.RGBA]
+
+    this.setState({ filters }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
+  }
+
+  changeColor = (color, amount) => {
+    this.setState({ [color]: this.state[color] + amount }, () => {
+      this.emoji.cache(konvaCacheConfig)
+    })
   }
 
   render () {
@@ -134,14 +196,24 @@ class Test extends Component {
                 y={0}
                 width={250}
                 height={250}
-                stroke={'black'}
+                stroke='black'
+                fill='white'
               />
               {this.state.chosenEmoji && (
                 <Text
+                  ref={ref => this.emoji = ref}
+                  filters={this.state.filters}
                   x={this.state.x}
                   y={this.state.y}
+                  scaleX={this.state.scaleX}
+                  scaleY={this.state.scaleY}
                   text={this.state.chosenEmoji}
                   fontSize={this.state.size}
+                  rotation={this.state.rotation}
+                  alpha={this.state.alpha}
+                  red={this.state.red}
+                  green={this.state.green}
+                  blue={this.state.blue}
                 />
               )}
             </Layer>
@@ -169,6 +241,49 @@ class Test extends Component {
             {/* SMALLER */}
             {this.state.chosenEmoji && <input type='button' onClick={this.makeSmaller} value='SMALLER' />}
           </CenteredButtons>
+
+          <CenteredButtons>
+            {/* ROTATION -> */}
+            {this.state.chosenEmoji && <input type='button' onClick={this.anticlockwiseRotation} value='ROTATE ->' />}
+            {/* ROTAION <- */}
+            {this.state.chosenEmoji && <input type='button' onClick={this.clockwiseRotation} value='ROTATE <-' />}
+          </CenteredButtons>
+
+          <CenteredButtons>
+            {/* FLIP X */}
+            {this.state.chosenEmoji && <input type='button' onClick={this.flipX} value='FLIP X' />}
+            {/* FLIP Y */}
+            {this.state.chosenEmoji && <input type='button' onClick={this.flipY} value='FLIP Y' />}
+          </CenteredButtons>
+
+          {/* TOGGLE FILTER*/}
+          {this.state.chosenEmoji && <input type='button' onClick={this.toggleFilter} value='TOGGLE FILTER' />}
+          {this.state.filters && (<React.Fragment>
+            <CenteredButtons>
+              {/* INCREASE EFFECT OF FILTER */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('alpha', .1)} value='INCREASE EFFECT' />}
+              {/* DECREASE EFFECT OF FILTER */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('alpha', -.1)} value='DECREASE EFFECT' />}
+            </CenteredButtons>
+            <CenteredButtons>
+              {/* INCREASE RED */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('red', 12)} value='INCREASE RED' />}
+              {/* DECREASE RED */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('red', -12)} value='DECREASE RED' />}
+            </CenteredButtons>
+            <CenteredButtons>
+              {/* INCREASE BLUE */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('blue', 12)} value='INCREASE BLUE' />}
+              {/* DECREASE BLUE */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('blue', -12)} value='DECREASE BLUE' />}
+            </CenteredButtons>
+            <CenteredButtons>
+              {/* INCREASE GREEN */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('green', 12)} value='INCREASE GREEN' />}
+              {/* DECREASE GREEN */}
+              {this.state.chosenEmoji && <input type='button' onClick={() => this.changeColor('green', -12)} value='DECREASE GREEN' />}
+            </CenteredButtons>
+          </React.Fragment>)}
 
           {this.state.showEmojiPicker && <EmojiPicker onSelect={this.onEmojiSelect} />}
         </CenteredContainer>

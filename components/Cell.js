702003d1bd50f2a24e9dd529402d4473e3b84259
Dean Slama <deanslamajr@gmail.com>
2019-01-29 19:21:36 -0700

    Added black outlined styles to Cell

--- components/Cell.js
+++ components/Cell.js
@@ -3,17 +3,28 @@ import styled from 'styled-components'
 
 const TitleContainer = styled.div`
   width: 250px;
+  background: #f9f9f9;;
+  border-top: black solid 1px;
+`
+
+const TitleWidth = styled.div`
+  margin-left: .25rem;
+  margin-right: .25rem;
 `
 
 const CellContainer = styled.div`
   margin: 1rem;
+  background: white;
+  border: black solid 1px;
 `
 
 export default function Cell ({ imageUrl, title }) {
   return (<CellContainer>
     <img src={imageUrl} />
     <TitleContainer>
-      {title}
+      <TitleWidth>
+        {title}
+      </TitleWidth>
     </TitleContainer>
   </CellContainer>)
 }

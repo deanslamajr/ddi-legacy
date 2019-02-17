242fab9b13c3f9741e46f9991241e07edcf348ba
Dean Slama <deanslamajr@gmail.com>
2019-02-17 09:02:16 -0700

    Added transaction to backfill migration so that 'too many connections' error doesn't occur for DBs with many rows in the Cells table, added use of array.prototype.reverse to correctly order the creation of rows in the Comic table during backfill migration

--- migrations/20190213001024-backfill-comic-for-each-existing-cell.js
+++ migrations/20190213001024-backfill-comic-for-each-existing-cell.js
@@ -3,18 +3,24 @@ const shortid = require('shortid')
 const { Cells } = require('../server/models')
 
 module.exports = {
-  up: async () => {
+  up: async (queryInterface) => {
+    return queryInterface.sequelize.transaction(async t => {
       // sorted by updated_at so that comic creation resembles this order
-      const cells = await Cells.findAll({ order: [['updated_at', 'DESC']] })
+      const cells = await Cells.findAll({ order: [['updated_at', 'DESC']] }, { transaction: t })
       const cellsWithoutAssociatedComic = cells.filter(cell => !cell.comic_id)
+      
+      // For some reason, cells is always in chronological order, regardless of the order directive above
+      // this one simple fix resorts the array into reverse chronological order (which is what we want!) 
+      cells.reverse()
       
       return Promise.all(cellsWithoutAssociatedComic.map(async (cell) => (
         cell.createComic({
           created_at: cell.created_at,
           title: '',
           url_id: shortid.generate()
-        })
+        }, { transaction: t })
       )))
+    })
   },
 
   down: () => Promise.resolve()

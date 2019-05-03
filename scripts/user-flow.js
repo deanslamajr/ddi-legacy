add9eb972d1a9478f796d301f170e219c18a8d10
Dean Slama <deanslamajr@gmail.com>
2019-05-02 21:00:10 -0600

    wip added user flow test

---
+++ scripts/user-flow.js
@@ -0,0 +1,56 @@
+const axios = require('axios');
+
+const ddiApi = 'https://app.drawdraw.ink/api'
+//const ddiApi = 'http://localhost:3000/api'
+
+let requestCount = 0
+
+function incrementRequestCount () {
+  requestCount++;
+  console.log('requestCount', requestCount)
+}
+
+async function doUserFlow () {
+  try {
+    // get comics
+    const {data: {comics}} = await axios.get(`${ddiApi}/comics`);
+    incrementRequestCount();
+    
+    const lookingAtAPageOfComics = comics.map(comic => {
+      return async () => {
+        console.log('comic', comic)
+        const {updated_at, url_id} = comic;
+
+        const {data: {cells}} = await axios.get(`${ddiApi}/comic/${url_id}`);
+        incrementRequestCount();
+        console.log('cells', cells);
+
+        const {data: cellData} = await axios.get(`${ddiApi}/cell/${cells[0].urlId}`);
+        incrementRequestCount();
+        console.log('cellData', cellData)
+
+        const {data} = await axios.get(`${ddiApi}/comics/latest?latestUpdatedAt=${comic.updated_at}`);
+        incrementRequestCount();
+        console.log('data', data)
+      }
+    })
+
+    for (let i = 0; i < lookingAtAPageOfComics.length; i++) {
+      await lookingAtAPageOfComics[i]()
+    }
+
+    // when out of comics, grab the next page and repeat
+    console.log('done')
+  }
+  catch(error) {
+    console.error(error)
+  }
+}
+
+async function userLoop () {
+  while(true) {
+    await doUserFlow();
+  }
+}
+
+userLoop();

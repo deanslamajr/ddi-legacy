c229a433d23af7fc4dab645c7e208eb15bf5baec
Dean Slama <deanslamajr@gmail.com>
2019-06-04 19:38:35 -0600

    reorganized studio component

--- pages/studio/index.js
+++ pages/studio/index.js
@@ -2,7 +2,6 @@ import { Router } from '../../routes'
 import { Component } from 'react'
 import styled from 'styled-components'
 import Konva from 'konva'
-import { Stage, Layer, Rect, Text, Image } from 'react-konva'
 import axios from 'axios'
 import shortid from 'shortid'
 import cloneDeep from 'lodash/cloneDeep'
@@ -18,39 +17,35 @@ import ActionsModal from './ActionsModal'
 import CaptionModal from './CaptionModal'
 import WarningModal from './WarningModal'
 import PreviewModal from './PreviewModal'
+import EmojiCanvas from './EmojiCanvas';
 
-import { getApi } from '../../helpers'
-import theme from '../../helpers/theme'
+import {
+  generateCellImage,
+  generateCellImageWithCaption,
+  konvaCacheConfig,
+  CELL_IMAGE_ID,
+  CELL_IMAGE_WITH_CAPTION_ID
+} from './konvaDrawingUtils'
+
+import { getApi, sortByOrder } from '../../helpers'
 
 import {
   S3_ASSET_FILETYPE,
   STORAGEKEY_STUDIO
 } from '../../config/constants.json'
 
-const CELL_IMAGE_ID = 'cell-image';
 const EMOJI_IMAGE_ID = 'emoji-image';
-const CELL_IMAGE_WITH_CAPTION_ID = 'cell-with-caption-image';
 
 const RGBA = 'RGBA'
 const filters = {
   [RGBA]: Konva.Filters.RGBA
 }
 
-const konvaCacheConfig = {
-  offset: 30,
-  pixelRatio: 1, /// fixes android graphics glitch
-  //drawBorder: true /// for debugging
-}
-
 function generateFilename () {
   return `${shortid.generate()}.png`
 }
 
-function sortByOrder ({ order: a }, { order: b }) {
-  return a - b
-}
-
-function createNewEmoji (emoji, currentEmojiId) {
+function createNewEmojiComponentState (emoji, currentEmojiId) {
   return {
     emoji,
     id: currentEmojiId,
@@ -71,195 +66,6 @@ function createNewEmoji (emoji, currentEmojiId) {
   }
 }
 
-/**
- * @todo step 1 generating emoji items as images
- */
-// async function getEmojiImageAsUrl () {  
-//   const emojiText = new Konva.Text({
-//     text: '🤖',
-//     text: '👩‍🏫',
-//     fontSize: 100
-//   });
-//   emojiText.lineHeight(1.1)
-
-//   console.log('emojiText.width()',emojiText.width())
-//   console.log('emojiText.height()',emojiText.height())
-
-//   const stage = new Konva.Stage({
-//     container: EMOJI_IMAGE_ID,
-//     // @TODO round these to integers
-//     width: emojiText.width(),
-//     height: emojiText.height()
-//   });
-
-//   const layer = new Konva.Layer();
-//   // add the layer to the stage
-//   stage.add(layer);
-//   layer.add(emojiText);
-
-//   const blob = await new Promise((resolve, reject) => {
-//     try {
-//       stage.toCanvas().toBlob((blob) => resolve(blob));
-//     }
-//     catch(err) {
-//       // @todo log error
-//       console.error(err);
-//       reject();
-//     }
-//   });
-
-//   const file = new File([blob], generateFilename(), {
-//     type: S3_ASSET_FILETYPE,
-//   });
-  
-//   return {
-//     url: URL.createObjectURL(file),
-//     file
-//   };
-// }
-
-function getCaptionConfig (title) {
-  return {
-    x: theme.finalImage.padding,
-    y: theme.finalImage.height + theme.finalImage.padding,
-    width: theme.finalImage.width - (2 * theme.finalImage.padding),
-    text: title,
-    fontSize: theme.finalImage.fontSize,
-    fill: theme.colors.black,
-    fontFamily: 'Calibri'
-  }
-}
-
-function getLinesOfCaptionText (title) {
-  if (title === '') {
-    return 0;
-  }
-  const captionConfig = getCaptionConfig(title);
-  const captionKonva = new Konva.Text(captionConfig);
-  return captionKonva.textArr.length;
-}
-
-function getEmojiConfigs (emojis) {
-  return emojis.sort(sortByOrder).map(emoji => ({
-    'data-id': emoji.id,
-    filters: emoji.filters && emoji.filters.map(filter => filters[filter]),
-    x: emoji.x,
-    y: emoji.y,
-    scaleX: emoji.scaleX,
-    scaleY: emoji.scaleY,
-    text: emoji.emoji,
-    fontSize: emoji.size,
-    rotation: emoji.rotation,
-    alpha: emoji.alpha,
-    red: emoji.red,
-    green: emoji.green,
-    blue: emoji.blue,
-    opacity: typeof emoji.opacity !== 'undefined' ? emoji.opacity : 1, /* backwards compatibility */
-    useCache: true
-  }));
-}
-
-function generateCellImageWithCaption(cellImageObj, title) {
-  const linesOfCaptionText = getLinesOfCaptionText(title);
-  const captionHeight = linesOfCaptionText
-    ? theme.finalImage.lineHeight * linesOfCaptionText + (2 * theme.finalImage.padding)
-    : 0;
-
-  const stageHeight = theme.finalImage.height + captionHeight;
-  const stageWidth = theme.finalImage.width;
-
-  const stage = new Konva.Stage({
-    container: CELL_IMAGE_WITH_CAPTION_ID,
-    width: stageWidth,
-    height: stageHeight
-  });
-
-  const layer = new Konva.Layer();
-  // add the layer to the stage
-  stage.add(layer);
-
-  const cellImage = new Konva.Image({
-    // width={1000}
-    // height={1000}
-    scale: {x:2, y:2},
-    x: 0,
-    y: 0,
-    image: cellImageObj
-  });
-      
-  layer.add(cellImage);
-
-  // Add caption background
-  if (linesOfCaptionText) {
-    const captionBackground = new Konva.Rect({
-      x: 0,
-      y: theme.finalImage.height,
-      width: theme.finalImage.width,
-      height: captionHeight,
-      fill: theme.colors.white
-    });
-    layer.add(captionBackground);
-  
-    {/* Caption text */}
-    const captionText = new Konva.Text({...getCaptionConfig(title)});
-    layer.add(captionText);
-  }
-
-  return new Promise((resolve, reject) => {
-    try {
-      stage.toCanvas().toBlob((blob) => resolve(blob));
-    }
-    catch(err) {
-      // @todo log error
-      console.error(err);
-      reject();
-    }
-  });
-}
-
-function generateCellImage (emojis) {
-  const stageHeight = theme.canvas.height;
-  const stageWidth = theme.canvas.width;
-
-  const stage = new Konva.Stage({
-    container: CELL_IMAGE_ID,
-    width: stageWidth,
-    height: stageHeight
-  });
-
-  const layer = new Konva.Layer();
-  // add the layer to the stage
-  stage.add(layer);
-  
-  // Add Canvas
-  const canvas = new Konva.Rect({
-    x: 0,
-    y: 0,
-    width: theme.canvas.width,
-    height: theme.canvas.height,
-    fill: theme.colors.white
-  });
-  layer.add(canvas);
-
-  // Add emojis
-  getEmojiConfigs(Object.values(emojis)).forEach(config => {
-    const emoji = new Konva.Text({...config});
-    emoji.cache(konvaCacheConfig)
-    layer.add(emoji);
-  });
-
-  return new Promise((resolve, reject) => {
-    try {
-      stage.toCanvas().toBlob((blob) => resolve(blob));
-    }
-    catch(err) {
-      // @todo log error
-      console.error(err);
-      reject();
-    }
-  });
-}
-
 function createImageFromUrl (url) {
   return new Promise((resolve, reject) => {
     const imageObj = new window.Image();
@@ -274,6 +80,11 @@ function createImageFromUrl (url) {
 //
 // Styled Components
 //
+const EverythingElseContainer = styled.div`
+  margin-top: ${props => props.theme.canvas.width + 5}px;
+  width: ${props => props.theme.canvas.width}px;
+`
+
 const CenteredContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -282,17 +93,6 @@ const CenteredContainer = styled.div`
   margin-bottom: 6rem;
   /* Hack to fix slider in fully right position making mobile view scroll :( */
   overflow-x: hidden;
-`
-
-const FixedCanvasContainer = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 999;
-`
-
-const EverythingElseContainer = styled.div`
-  margin-top: ${props => props.theme.canvas.width + 5}px;
-  width: ${props => props.theme.canvas.width}px;
 `
 
 //
@@ -377,7 +177,7 @@ class StudioRoute extends Component {
 
   createNewEmoji = (emoji) => {
     this.setState(({ currentEmojiId, emojis }) => {
-      const newEmoji = createNewEmoji(emoji, currentEmojiId)
+      const newEmoji = createNewEmojiComponentState(emoji, currentEmojiId)
 
       const clonedEmojis = cloneDeep(emojis)
       clonedEmojis[newEmoji.id] = newEmoji
@@ -892,49 +692,13 @@ class StudioRoute extends Component {
         <div style={{display: 'none'}} id={CELL_IMAGE_WITH_CAPTION_ID} />
         <div style={{display: 'none'}} id={EMOJI_IMAGE_ID} />
         {this.state.emojiUrl && <img url={this.state.emojiUrl.url} />}
-
         <CenteredContainer>
-          <FixedCanvasContainer>
-            <Stage
-              ref={ref => this.stage = ref}
-              width={theme.canvas.width}
-              height={theme.canvas.height}
-            >
-              <Layer>
-                {/* Canvas */}
-                <Rect
-                  x={0}
-                  y={0}
-                  width={theme.canvas.width}
-                  height={theme.canvas.height}
-                  fill={theme.colors.white}
-                />
-
-                {/* /**
-                  * @todo step 3 generating emoji items as images
-                */}
-                {/* {this.state.emojiImageObj && <Image
-                  draggable
-                  rotation={0}
-                  width={1000}
-                  height={1000}
-                  x={0}
-                  y={0}
-                  image={this.state.emojiImageObj}
-                />} */}
-
-                {getEmojiConfigs(Object.values(this.state.emojis)).map(config => <Text
-                  draggable={config['data-id'] === this.state.activeEmojiId}
-                  useCache
-                  key={`${config['data-id']}${config.emoji}`}
-                  ref={ref => this.emojiRefs[config['data-id']] = ref}
-                  onDragEnd={this.handleDragEnd}
-                  {...config}
-                />)}
-              </Layer>
-            </Stage>
-          </FixedCanvasContainer>
-
+          <EmojiCanvas
+            activeEmojiId={this.state.activeEmojiId}
+            emojis={this.state.emojis}
+            emojiRefs={this.emojiRefs}
+            handleDragEnd={this.handleDragEnd}
+          />
           {this.state.showSaveButton && (
             <EverythingElseContainer>
               <BuilderMenu
@@ -958,7 +722,8 @@ class StudioRoute extends Component {
                 onCancel={this.onPickerCancel}
                 backButtonLabel={this.state.activeEmojiId ? 'BACK' : 'EXIT'}
               />}
-            </EverythingElseContainer>)}
+            </EverythingElseContainer>
+          )}
         </CenteredContainer>
 
         {!this.state.showEmojiPicker && <React.Fragment>
---
+++ pages/studio/konvaDrawingUtils.js
@@ -0,0 +1,207 @@
+import Konva from 'konva'
+
+import theme from '../../helpers/theme'
+import { getApi, sortByOrder } from '../../helpers'
+
+function getLinesOfCaptionText (title) {
+  if (title === '') {
+    return 0;
+  }
+  const captionConfig = getCaptionConfig(title);
+  const captionKonva = new Konva.Text(captionConfig);
+  return captionKonva.textArr.length;
+}
+
+/**
+ * EXPORTS
+ ****
+ ***
+ **
+ */
+
+export const CELL_IMAGE_WITH_CAPTION_ID = 'cell-with-caption-image';
+export const CELL_IMAGE_ID = 'cell-image';
+
+export const konvaCacheConfig = {
+  offset: 30,
+  pixelRatio: 1, /// fixes android graphics glitch
+  //drawBorder: true /// for debugging
+}
+
+export function getCaptionConfig (title) {
+  return {
+    x: theme.finalImage.padding,
+    y: theme.finalImage.height + theme.finalImage.padding,
+    width: theme.finalImage.width - (2 * theme.finalImage.padding),
+    text: title,
+    fontSize: theme.finalImage.fontSize,
+    fill: theme.colors.black,
+    fontFamily: 'Calibri'
+  }
+}
+
+export function generateCellImageWithCaption(cellImageObj, title) {
+  const linesOfCaptionText = getLinesOfCaptionText(title);
+  const captionHeight = linesOfCaptionText
+    ? theme.finalImage.lineHeight * linesOfCaptionText + (2 * theme.finalImage.padding)
+    : 0;
+
+  const stageHeight = theme.finalImage.height + captionHeight;
+  const stageWidth = theme.finalImage.width;
+
+  const stage = new Konva.Stage({
+    container: CELL_IMAGE_WITH_CAPTION_ID,
+    width: stageWidth,
+    height: stageHeight
+  });
+
+  const layer = new Konva.Layer();
+  // add the layer to the stage
+  stage.add(layer);
+
+  const cellImage = new Konva.Image({
+    scale: {x:2, y:2},
+    x: 0,
+    y: 0,
+    image: cellImageObj
+  });
+      
+  layer.add(cellImage);
+
+  // Add caption background
+  if (linesOfCaptionText) {
+    const captionBackground = new Konva.Rect({
+      x: 0,
+      y: theme.finalImage.height,
+      width: theme.finalImage.width,
+      height: captionHeight,
+      fill: theme.colors.white
+    });
+    layer.add(captionBackground);
+  
+    {/* Caption text */}
+    const captionText = new Konva.Text({...getCaptionConfig(title)});
+    layer.add(captionText);
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      stage.toCanvas().toBlob((blob) => resolve(blob));
+    }
+    catch(err) {
+      // @todo log error
+      console.error(err);
+      reject();
+    }
+  });
+}
+
+export function getEmojiConfigs (emojis) {
+  return emojis.sort(sortByOrder).map(emoji => ({
+    'data-id': emoji.id,
+    filters: emoji.filters && emoji.filters.map(filter => filters[filter]),
+    x: emoji.x,
+    y: emoji.y,
+    scaleX: emoji.scaleX,
+    scaleY: emoji.scaleY,
+    text: emoji.emoji,
+    fontSize: emoji.size,
+    rotation: emoji.rotation,
+    alpha: emoji.alpha,
+    red: emoji.red,
+    green: emoji.green,
+    blue: emoji.blue,
+    opacity: typeof emoji.opacity !== 'undefined' ? emoji.opacity : 1, /* backwards compatibility */
+    useCache: true
+  }));
+}
+
+export function generateCellImage (emojis) {
+  const stageHeight = theme.canvas.height;
+  const stageWidth = theme.canvas.width;
+
+  const stage = new Konva.Stage({
+    container: CELL_IMAGE_ID,
+    width: stageWidth,
+    height: stageHeight
+  });
+
+  const layer = new Konva.Layer();
+  // add the layer to the stage
+  stage.add(layer);
+  
+  // Add Canvas
+  const canvas = new Konva.Rect({
+    x: 0,
+    y: 0,
+    width: theme.canvas.width,
+    height: theme.canvas.height,
+    fill: theme.colors.white
+  });
+  layer.add(canvas);
+
+  // Add emojis
+  getEmojiConfigs(Object.values(emojis)).forEach(config => {
+    const emoji = new Konva.Text({...config});
+    emoji.cache(konvaCacheConfig)
+    layer.add(emoji);
+  });
+
+  return new Promise((resolve, reject) => {
+    try {
+      stage.toCanvas().toBlob((blob) => resolve(blob));
+    }
+    catch(err) {
+      // @todo log error
+      console.error(err);
+      reject();
+    }
+  });
+}
+
+/**
+ * @todo step 1 generating emoji items as images
+ */
+// async function getEmojiImageAsUrl () {  
+//   const emojiText = new Konva.Text({
+//     text: '🤖',
+//     text: '👩‍🏫',
+//     fontSize: 100
+//   });
+//   emojiText.lineHeight(1.1)
+
+//   console.log('emojiText.width()',emojiText.width())
+//   console.log('emojiText.height()',emojiText.height())
+
+//   const stage = new Konva.Stage({
+//     container: EMOJI_IMAGE_ID,
+//     // @TODO round these to integers
+//     width: emojiText.width(),
+//     height: emojiText.height()
+//   });
+
+//   const layer = new Konva.Layer();
+//   // add the layer to the stage
+//   stage.add(layer);
+//   layer.add(emojiText);
+
+//   const blob = await new Promise((resolve, reject) => {
+//     try {
+//       stage.toCanvas().toBlob((blob) => resolve(blob));
+//     }
+//     catch(err) {
+//       // @todo log error
+//       console.error(err);
+//       reject();
+//     }
+//   });
+
+//   const file = new File([blob], generateFilename(), {
+//     type: S3_ASSET_FILETYPE,
+//   });
+  
+//   return {
+//     url: URL.createObjectURL(file),
+//     file
+//   };
+// }

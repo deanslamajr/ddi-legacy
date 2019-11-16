7456b7f32b4e571cfcef473ed5a219152cea37cc
Dean Slama <deanslamajr@gmail.com>
2019-11-16 12:04:53 -0700

    added use of CellPreviewMetaTags on /cell/:cellUrlId

--- pages/cell.js
+++ pages/cell.js
@@ -9,6 +9,7 @@ import {
   BOTTOM_LEFT,
   BOTTOM_RIGHT
 } from '../components/navigation'
+import {CellPreviewMetaTags} from '../components/CellPreviewMetaTags'
 
 import { Router } from '../routes'
 import { getApi } from '../helpers'
@@ -75,26 +76,12 @@ class CellRoute extends Component {
       <div>
         <Head>
           <title>DrawDrawInk - {caption ? `${caption}` : 'Cell'}</title>
-          {/* - iMessage/Messages https://developer.apple.com/library/archive/technotes/tn2444/_index.html
-                * Images should be at least 900px in width
-              - android app Messages
-                * rectangular preview image. 900*350 seems to work here
-                * 23 characters max for caption  */}
-          <meta property="og:site_name" content="drawdrawink" />
-          <meta property="og:title" content={caption} />
-          <meta property="og:image" content={imageUrl} />
-          {/* The link preview generation will look for an apple-touch-icon, favicon, or one specified by <link rel="...">. 
-              Icons should be square, and at least 108px per side. */}
-          {/* <link rel="apple-touch-icon" href="https://www.link.to/icon/appIcon.png"> */}
-
-          {/* - platforms: Twitter https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary.html */}
-          <meta name="twitter:card" content={caption} />
-          {/* <meta name="twitter:site" content="@flickr" /> */}
-          <meta name="twitter:title" content={caption} />
-          {/* <meta name="twitter:description" content="1 View the album on Flickr. 2 View the album on Flickr. 3 View the album on Flickr. 4 View the album on Flickr. 5 View the album on Flickr. 6 View the album on Flickr. 7 View the album on Flickr. 8 View the album on Flickr. 9 View the album on Flickr. 0 View the album on Flickr." /> */}
-          {/* Images for this Card support an aspect ratio of 1:1 with minimum dimensions of 144x144 or maximum of 4096x4096 pixels. Images must be less than 5MB in size. The image will be cropped to a square on all platforms. JPG, PNG, WEBP and GIF formats are supported. Only the first frame of an animated GIF will be used. SVG is not supported  */}
-          <meta name="twitter:image" content={imageUrl} />
         </Head>
+        <CellPreviewMetaTags
+          caption={caption || ' '}
+          imageUrl={imageUrl}
+          schemaVersion={schemaVersion}
+        />
 
         <CenteredContainer>
           <Cell imageUrl={imageUrl} caption={caption} schemaVersion={schemaVersion}/>

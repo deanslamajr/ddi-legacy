cc38165c6d575b287d8592a83c5de78bdb00bf3f
Dean Slama <deanslamajr@gmail.com>
2023-02-04 14:29:49 -0700

    expanded MAX_SIZE to match ddi_nui, prettier autoformatting

--- pages/s/cell/BuilderMenu.js
+++ pages/s/cell/BuilderMenu.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
-import rgbHex from 'rgb-hex';
-import hexRgb from 'hex-rgb';
+import React from 'react'
+import styled from 'styled-components'
+import rgbHex from 'rgb-hex'
+import hexRgb from 'hex-rgb'
 
 import EmojiEditModal from './EmojiEditModal'
 
-import {Spacer} from '../../../components/Spacer';
+import { Spacer } from '../../../components/Spacer'
 import NewSlider from '../../../components/NewSlider'
 import {
   MenuButton,
   PinkMenuButton,
-  DisabledButton
+  DisabledButton,
 } from '../../../components/Buttons'
 
 import theme from '../../../helpers/theme'
@@ -27,7 +27,7 @@ import {
   MAX_OPACITY,
   MIN_ALPHA,
   MAX_ALPHA,
-  MAX_EMOJIS_COUNT
+  MAX_EMOJIS_COUNT,
 } from '../../../config/constants.json'
 
 // function toRgb (hex) {
@@ -38,10 +38,10 @@ import {
 //   return {r,g,b};
 // }
 
-const toHex = ({r, g, b}) => `#${rgbHex(r, g, b)}`;
+const toHex = ({ r, g, b }) => `#${rgbHex(r, g, b)}`
 const toRgb = (hex) => {
-  const {red: r, green: g, blue: b} = hexRgb(hex.substr(1));
-  return {r, g, b};
+  const { red: r, green: g, blue: b } = hexRgb(hex.substr(1))
+  return { r, g, b }
 }
 
 const ColorPicker = styled.input`
@@ -61,7 +61,7 @@ const CenteredButtons = styled.div`
 
 const SliderContainer = styled.div`
   width: 100%;
-  padding: .5rem 0 1.5rem;
+  padding: 0.5rem 0 1.5rem;
 `
 
 const HalfMenuButton = styled(MenuButton)`
@@ -69,23 +69,24 @@ const HalfMenuButton = styled(MenuButton)`
 `
 
 const SelectActiveEmojiButton = styled(MenuButton)`
-  background-color: ${props => props.theme.colors.white};
-  border: ${props => props.isActive ? `2px solid ${props.theme.colors.pink}` : 'none'};
-  cursor: ${props => props.isActive ? 'default' : 'pointer'};
+  background-color: ${(props) => props.theme.colors.white};
+  border: ${(props) =>
+    props.isActive ? `2px solid ${props.theme.colors.pink}` : 'none'};
+  cursor: ${(props) => (props.isActive ? 'default' : 'pointer')};
   font-size: 2rem;
-  height: ${props => props.isActive ? 'calc(2.75rem - 4px)' : '2.75rem'};
-  width: ${props => props.isActive ? '246px' : '250px'};
+  height: ${(props) => (props.isActive ? 'calc(2.75rem - 4px)' : '2.75rem')};
+  width: ${(props) => (props.isActive ? '246px' : '250px')};
   display: flex;
   justify-content: space-between;
 
   &:hover {
-    background-color: ${props => props.theme.colors.black};
-    color: ${props => props.theme.colors.white};
+    background-color: ${(props) => props.theme.colors.black};
+    color: ${(props) => props.theme.colors.white};
   }
 `
 
 const Label = styled.span`
-  margin: .1rem auto;
+  margin: 0.1rem auto;
   display: flex;
   justify-content: center;
   user-select: none;
@@ -105,17 +106,20 @@ const EmojiContainer = styled(ThirdOfAButton)`
 `
 
 const ChangeLayerButton = styled(ThirdOfAButton)`
-  border: 1px solid ${props => props.theme.colors.darkGray};
-  border-right: ${props => !props.isLeft && props.isActive ? 'none' : `1px solid ${props.theme.colors.darkGray}`};
-  width: ${props => props.isActive ? 'calc(20% - 1px)' : '20%'};
-  height: ${props => props.isActive ? 'calc(2.75rem - 6px)' : '2.75rem'};
-  background-color: ${props => props.theme.colors.white};
-  color: ${props => props.theme.colors.darkGray};
+  border: 1px solid ${(props) => props.theme.colors.darkGray};
+  border-right: ${(props) =>
+    !props.isLeft && props.isActive
+      ? 'none'
+      : `1px solid ${props.theme.colors.darkGray}`};
+  width: ${(props) => (props.isActive ? 'calc(20% - 1px)' : '20%')};
+  height: ${(props) => (props.isActive ? 'calc(2.75rem - 6px)' : '2.75rem')};
+  background-color: ${(props) => props.theme.colors.white};
+  color: ${(props) => props.theme.colors.darkGray};
   cursor: pointer;
 
   &:hover {
-    background-color: ${props => props.theme.colors.pink};
-    color: ${props => props.theme.colors.black};
+    background-color: ${(props) => props.theme.colors.pink};
+    color: ${(props) => props.theme.colors.black};
   }
 `
 
@@ -124,16 +128,16 @@ const AddEmojiButton = styled(PinkMenuButton)`
 `
 
 const NudgeButton = styled(HalfMenuButton)`
-  font-size: 2rem; 
+  font-size: 2rem;
   margin: 1px auto;
-`;
+`
 
 const MAIN = 'MAIN'
 const SECONDARY = 'SECONDARY'
 const FILTERS = 'FILTERS'
-const BG_COLOR = 'BG_COLOR';
+const BG_COLOR = 'BG_COLOR'
 
-function getOpacity (activeEmoji) {
+function getOpacity(activeEmoji) {
   const defaultOpacity = 1
 
   // Backwards compatibility
@@ -145,19 +149,19 @@ function getOpacity (activeEmoji) {
 }
 
 class BuilderMenu extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
 
     this.state = {
       currentMenu: MAIN,
-      showEmojiEditModal: false
+      showEmojiEditModal: false,
     }
 
     this.menus = {
       [MAIN]: this.renderMainMenu,
       [SECONDARY]: this.renderSecondaryMenu,
       [FILTERS]: this.renderFiltersMenu,
-      [BG_COLOR]: this.renderBackgroundColorMenu
+      [BG_COLOR]: this.renderBackgroundColorMenu,
     }
   }
 
@@ -180,273 +184,290 @@ class BuilderMenu extends React.Component {
   onDragEnd = (a, b) => {
     // Hack to force the canvas to redraw with the latest active emoji values
     // Need to pass a new value to a field that is passed to the konva <Text /> instance associated with the active emoji
-    this.props.setField('red', this.props.activeEmoji.red + .01)
+    this.props.setField('red', this.props.activeEmoji.red + 0.01)
   }
 
   renderMainMenu = () => {
-    const {
-      activeEmoji,
-      changeActiveEmoji,
-      emojis,
-      setField
-    } = this.props
+    const { activeEmoji, changeActiveEmoji, emojis, setField } = this.props
 
     const emojisArray = Object.values(emojis)
     const canAddEmojis = emojisArray.length <= MAX_EMOJIS_COUNT
 
-    return (<React.Fragment>
-      {activeEmoji && <React.Fragment>
-        <SliderContainer>
-          <Label>SIZE</Label>
-          <NewSlider
-            min={MIN_SIZE}
-            max={MAX_SIZE}
-            step={1}
-            value={(activeEmoji && activeEmoji.size) || 0}
-            onChange={(value) => setField('size', value)}
-          />
-        </SliderContainer>
-        <SliderContainer>
-          <Label>ROTATION</Label>
-          <NewSlider
-            min={MIN_ROTATION}
-            max={MAX_ROTATION}
-            step={1}
-            value={(activeEmoji && activeEmoji.rotation) || 0}
-            onChange={(value) => setField('rotation', value)}
-          />
-        </SliderContainer>
+    return (
+      <React.Fragment>
+        {activeEmoji && (
+          <React.Fragment>
+            <SliderContainer>
+              <Label>SIZE</Label>
+              <NewSlider
+                min={MIN_SIZE}
+                max={MAX_SIZE / 2}
+                step={1}
+                value={(activeEmoji && activeEmoji.size) || 0}
+                onChange={(value) => setField('size', value)}
+              />
+            </SliderContainer>
+            <SliderContainer>
+              <Label>ROTATION</Label>
+              <NewSlider
+                min={MIN_ROTATION}
+                max={MAX_ROTATION}
+                step={1}
+                value={(activeEmoji && activeEmoji.rotation) || 0}
+                onChange={(value) => setField('rotation', value)}
+              />
+            </SliderContainer>
 
-        <MenuButton onClick={() => this.setState({ currentMenu: SECONDARY })}>
-          ADVANCED
-        </MenuButton>
-      </React.Fragment>}
+            <MenuButton
+              onClick={() => this.setState({ currentMenu: SECONDARY })}
+            >
+              ADVANCED
+            </MenuButton>
+          </React.Fragment>
+        )}
 
-      <Spacer/>
-      
-      {canAddEmojis
-        ? (<AddEmojiButton onClick={this.props.openEmojiPicker}>
+        <Spacer />
+
+        {canAddEmojis ? (
+          <AddEmojiButton onClick={this.props.openEmojiPicker}>
             +
-          </AddEmojiButton>)
-        : (<DisabledButton >
-            EMOJI LIMIT REACHED
-          </DisabledButton>)
-      }
+          </AddEmojiButton>
+        ) : (
+          <DisabledButton>EMOJI LIMIT REACHED</DisabledButton>
+        )}
 
-      {emojisArray.sort(sortByOrder).reverse().map(({ emoji, id }, index) => (<SelectActiveEmojiButton
-        key={`${id}${emoji}`}
-        type='button'
-        isActive={id === activeEmoji.id}
-        onClick={() => id === activeEmoji.id ? this.toggleEmojiEditModal(true) : changeActiveEmoji(id)}
-        value={emoji}
-      >
-        {index !== 0
-          ? (<ChangeLayerButton
+        {emojisArray
+          .sort(sortByOrder)
+          .reverse()
+          .map(({ emoji, id }, index) => (
+            <SelectActiveEmojiButton
+              key={`${id}${emoji}`}
+              type="button"
               isActive={id === activeEmoji.id}
-              onClick={(e) => this.increaseStackOrder(e, id)}
-              isLeft
+              onClick={() =>
+                id === activeEmoji.id
+                  ? this.toggleEmojiEditModal(true)
+                  : changeActiveEmoji(id)
+              }
+              value={emoji}
             >
-              ↑
-            </ChangeLayerButton>)
-          : <ThirdOfAButton/>}
-        <EmojiContainer>{emoji}</EmojiContainer>
-        {index !== emojisArray.length - 1 
-          ? (<ChangeLayerButton
-              isActive={id === activeEmoji.id}
-              onClick={(e) => this.decreaseStackOrder(e, id)}
-            >
-              ↓
-            </ChangeLayerButton>)
-          : <ThirdOfAButton/>}
-      </SelectActiveEmojiButton>))}
-    </React.Fragment>)
+              {index !== 0 ? (
+                <ChangeLayerButton
+                  isActive={id === activeEmoji.id}
+                  onClick={(e) => this.increaseStackOrder(e, id)}
+                  isLeft
+                >
+                  ↑
+                </ChangeLayerButton>
+              ) : (
+                <ThirdOfAButton />
+              )}
+              <EmojiContainer>{emoji}</EmojiContainer>
+              {index !== emojisArray.length - 1 ? (
+                <ChangeLayerButton
+                  isActive={id === activeEmoji.id}
+                  onClick={(e) => this.decreaseStackOrder(e, id)}
+                >
+                  ↓
+                </ChangeLayerButton>
+              ) : (
+                <ThirdOfAButton />
+              )}
+            </SelectActiveEmojiButton>
+          ))}
+      </React.Fragment>
+    )
   }
 
   showCanvaColorMenu = () => {
-    this.setState({ currentMenu: BG_COLOR });
-    this.props.hideActionsMenu();
+    this.setState({ currentMenu: BG_COLOR })
+    this.props.hideActionsMenu()
   }
 
   renderSecondaryMenu = () => {
-    const {
-      activeEmoji,
-      incrementField,
-      scaleField,
-      setField
-    } = this.props
+    const { activeEmoji, incrementField, scaleField, setField } = this.props
 
-    return (<React.Fragment>
-      <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
-        SIMPLE
-      </PinkMenuButton>
+    return (
+      <React.Fragment>
+        <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
+          SIMPLE
+        </PinkMenuButton>
 
-      <Spacer/>
+        <Spacer />
 
-      <MenuButton onClick={() => this.setState({ currentMenu: FILTERS })}>
-        FILTERS
-      </MenuButton>
+        <MenuButton onClick={() => this.setState({ currentMenu: FILTERS })}>
+          FILTERS
+        </MenuButton>
 
-      <Spacer/>
+        <Spacer />
 
-      <Label>SKEW X</Label>
+        <Label>SKEW X</Label>
         <NewSlider
           min={MIN_SKEW}
           max={MAX_SKEW}
-          step={.05}
+          step={0.05}
           value={(activeEmoji && activeEmoji.skewX) || 0}
           onChange={(value) => setField('skewX', value)}
         />
-      <Label>SKEW Y</Label>
+        <Label>SKEW Y</Label>
         <NewSlider
           min={MIN_SKEW}
           max={MAX_SKEW}
-          step={.05}
+          step={0.05}
           value={(activeEmoji && activeEmoji.skewY) || 0}
           onChange={(value) => setField('skewY', value)}
         />
 
-      <Spacer/>
+        <Spacer />
 
-      <Label>FLIP</Label>
-      <CenteredButtons>
-        <HalfMenuButton onClick={() => scaleField('scaleX', -1)}>
-          X
-        </HalfMenuButton>
-        <HalfMenuButton onClick={() => scaleField('scaleY', -1)}>
-          Y
-        </HalfMenuButton>
-      </CenteredButtons>
+        <Label>FLIP</Label>
+        <CenteredButtons>
+          <HalfMenuButton onClick={() => scaleField('scaleX', -1)}>
+            X
+          </HalfMenuButton>
+          <HalfMenuButton onClick={() => scaleField('scaleY', -1)}>
+            Y
+          </HalfMenuButton>
+        </CenteredButtons>
 
-      <Spacer/>
+        <Spacer />
 
-      <CenteredButtons>
-        <NudgeButton onClick={() => incrementField('y', -1)}>
-          ↑
-        </NudgeButton>
-      </CenteredButtons>
-      <CenteredButtons>
-        <NudgeButton onClick={() => incrementField('x', -1)}>
-          ←
-        </NudgeButton>
-        <NudgeButton onClick={() => incrementField('x', 1)}>
-          →
-        </NudgeButton>
-      </CenteredButtons>
-      <CenteredButtons>
-        <NudgeButton onClick={() => incrementField('y', 1)}>
-          ↓
-        </NudgeButton>
-      </CenteredButtons>
-    </React.Fragment>)
+        <CenteredButtons>
+          <NudgeButton onClick={() => incrementField('y', -1)}>↑</NudgeButton>
+        </CenteredButtons>
+        <CenteredButtons>
+          <NudgeButton onClick={() => incrementField('x', -1)}>←</NudgeButton>
+          <NudgeButton onClick={() => incrementField('x', 1)}>→</NudgeButton>
+        </CenteredButtons>
+        <CenteredButtons>
+          <NudgeButton onClick={() => incrementField('y', 1)}>↓</NudgeButton>
+        </CenteredButtons>
+      </React.Fragment>
+    )
   }
 
   renderBackgroundColorMenu = () => {
-    return (<React.Fragment>
-      <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
-        BACK
-      </PinkMenuButton>
+    return (
+      <React.Fragment>
+        <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
+          BACK
+        </PinkMenuButton>
 
-      <Spacer/>
+        <Spacer />
 
-      <ColorPicker
-        type='color'
-        value={this.props.backgroundColor}
-        onChange={e => this.props.onColorChange(e.target.value)}
-      />
+        <ColorPicker
+          type="color"
+          value={this.props.backgroundColor}
+          onChange={(e) => this.props.onColorChange(e.target.value)}
+        />
 
-      <Spacer/>
+        <Spacer />
 
-      <MenuButton onClick={() => this.props.onColorChange(theme.colors.white)}>
-        RESET
-      </MenuButton>
-    </React.Fragment>);
+        <MenuButton
+          onClick={() => this.props.onColorChange(theme.colors.white)}
+        >
+          RESET
+        </MenuButton>
+      </React.Fragment>
+    )
   }
 
   renderFiltersMenu = () => {
-    const {
-      activeEmoji,
-      setField,
-      toggleFilter
-    } = this.props
+    const { activeEmoji, setField, toggleFilter } = this.props
 
     const rgb = activeEmoji
       ? { r: activeEmoji.red, g: activeEmoji.green, b: activeEmoji.blue }
-      : { r: 0, g: 0, b: 0 };
+      : { r: 0, g: 0, b: 0 }
 
-    return (<React.Fragment>
-      <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
-        BACK
-      </PinkMenuButton>
+    return (
+      <React.Fragment>
+        <PinkMenuButton onClick={() => this.setState({ currentMenu: MAIN })}>
+          BACK
+        </PinkMenuButton>
 
-      <Spacer/>
+        <Spacer />
 
-      {activeEmoji.filters && (<React.Fragment>          
-        <ColorPicker
-          type='color'
-          value={toHex(rgb)}
-          onChange={e => this.props.setFilterColor(toRgb(e.target.value))}
-        />
-        
-        <Spacer/>
+        {activeEmoji.filters && (
+          <React.Fragment>
+            <ColorPicker
+              type="color"
+              value={toHex(rgb)}
+              onChange={(e) => this.props.setFilterColor(toRgb(e.target.value))}
+            />
+
+            <Spacer />
+
+            <SliderContainer>
+              <Label>AMOUNT</Label>
+              <NewSlider
+                min={MIN_ALPHA}
+                max={MAX_ALPHA}
+                step={0.01}
+                value={(activeEmoji && activeEmoji.alpha) || 0}
+                onChange={(value) => setField('alpha', value)}
+              />
+            </SliderContainer>
+          </React.Fragment>
+        )}
+
+        {/* TOGGLE FILTER*/}
+        <MenuButton onClick={toggleFilter}>
+          {activeEmoji.filters ? 'DISABLE COLOR' : 'ENABLE COLOR'}
+        </MenuButton>
+
+        <Spacer />
 
         <SliderContainer>
-          <Label>AMOUNT</Label>
+          <Label>OPACITY</Label>
           <NewSlider
-            min={MIN_ALPHA}
-            max={MAX_ALPHA}
-            step={.01}
-            value={(activeEmoji && activeEmoji.alpha) || 0}
-            onChange={(value) => setField('alpha', value)}
+            min={MIN_OPACITY}
+            max={MAX_OPACITY}
+            step={0.01}
+            value={getOpacity(activeEmoji)}
+            onChange={(value) => setField('opacity', value)}
           />
         </SliderContainer>
-      </React.Fragment>)}
-
-      {/* TOGGLE FILTER*/}
-      <MenuButton onClick={toggleFilter}>{activeEmoji.filters ? 'DISABLE COLOR' : 'ENABLE COLOR'}</MenuButton>
-    
-      <Spacer/>
-
-      <SliderContainer>
-        <Label>OPACITY</Label>
-        <NewSlider
-          min={MIN_OPACITY}
-          max={MAX_OPACITY}
-          step={.01}
-          value={getOpacity(activeEmoji)}
-          onChange={(value) => setField('opacity', value)}
-        />
-      </SliderContainer>
-    </React.Fragment>)
+      </React.Fragment>
+    )
   }
 
   toggleEmojiEditModal = (newValue) => {
     this.setState({ showEmojiEditModal: newValue })
   }
 
-  render () {
+  render() {
     const {
       activeEmoji,
       onChangeClick,
       onDeleteClick,
       onDuplicateClick,
       renderActionsMenu,
-      isActionsModalVisible
+      isActionsModalVisible,
     } = this.props
     const { showEmojiEditModal } = this.state
 
     return (
-      <React.Fragment>       
+      <React.Fragment>
         {this.menus[this.state.currentMenu]()}
 
-        {showEmojiEditModal && <EmojiEditModal
-          emoji={activeEmoji}
-          onCancelClick={() => this.toggleEmojiEditModal(false)}
-          onChangeClick={() => onChangeClick(this.toggleEmojiEditModal.bind(this, false))}
-          onDeleteClick={() => onDeleteClick(this.toggleEmojiEditModal.bind(this, false))}
-          onDuplicateClick={() => onDuplicateClick(this.toggleEmojiEditModal.bind(this, false))}
-        />}
+        {showEmojiEditModal && (
+          <EmojiEditModal
+            emoji={activeEmoji}
+            onCancelClick={() => this.toggleEmojiEditModal(false)}
+            onChangeClick={() =>
+              onChangeClick(this.toggleEmojiEditModal.bind(this, false))
+            }
+            onDeleteClick={() =>
+              onDeleteClick(this.toggleEmojiEditModal.bind(this, false))
+            }
+            onDuplicateClick={() =>
+              onDuplicateClick(this.toggleEmojiEditModal.bind(this, false))
+            }
+          />
+        )}
 
-        {isActionsModalVisible && renderActionsMenu({showCanvaColorMenu: this.showCanvaColorMenu})}
+        {isActionsModalVisible &&
+          renderActionsMenu({ showCanvaColorMenu: this.showCanvaColorMenu })}
       </React.Fragment>
     )
   }
--- pages/s/cell/EmojiCanvas.js
+++ pages/s/cell/EmojiCanvas.js
@@ -1,10 +1,14 @@
-import { Component } from 'react';
-import styled from 'styled-components';
-import { Stage, Layer, Rect, Text, Group } from 'react-konva';
+import { Component } from 'react'
+import styled from 'styled-components'
+import { Stage, Layer, Rect, Text, Group } from 'react-konva'
 
-import theme from '../../../helpers/theme';
+import theme from '../../../helpers/theme'
 
-import {getEmojiConfigs, EMOJI_MASK_REF_ID, EMOJI_MASK_OUTLINE_REF_ID} from '../../../helpers/konvaDrawingUtils'
+import {
+  getEmojiConfigs,
+  EMOJI_MASK_REF_ID,
+  EMOJI_MASK_OUTLINE_REF_ID,
+} from '../../../helpers/konvaDrawingUtils'
 
 //
 // Styled Components
@@ -16,15 +20,7 @@ const FixedCanvasContainer = styled.div`
 `
 
 function getOutlineConfig(config = {}) {
-  const {
-    x,
-    y,
-    scaleX,
-    scaleY,
-    rotation,
-    fontSize,
-    useCache
-  } = config;
+  const { x, y, scaleX, scaleY, rotation, fontSize, useCache } = config
 
   return {
     x,
@@ -34,7 +30,7 @@ function getOutlineConfig(config = {}) {
     rotation,
     text: '    ',
     fontSize,
-    useCache
+    useCache,
   }
 }
 
@@ -42,66 +38,67 @@ class EmojiCanvas extends Component {
   state = {
     isDragging: false,
     prevX: 0,
-    prevY: 0
+    prevY: 0,
   }
 
   onDragStart = (e) => {
-    this.setState({isDragging: true})
+    this.setState({ isDragging: true })
   }
 
   onDragEnd = (e) => {
-    const { x, y } = e.target.attrs;
-    const {prevX, prevY} = this.state;
+    const { x, y } = e.target.attrs
+    const { prevX, prevY } = this.state
 
-    const xDiff = x - prevX;
-    const yDiff = y - prevY;
+    const xDiff = x - prevX
+    const yDiff = y - prevY
 
-    this.props.handleDragEnd({xDiff, yDiff});
+    this.props.handleDragEnd({ xDiff, yDiff })
 
     // we need a change to force a rerender of Group (to reset the position of draggable group)
-    const groupXChange = xDiff > 0 ? .01 : -.01;
-    const groupYChange = yDiff > 0 ? .01 : -.01;
+    const groupXChange = xDiff > 0 ? 0.01 : -0.01
+    const groupYChange = yDiff > 0 ? 0.01 : -0.01
 
     this.setState({
       isDragging: false,
       prevX: this.state.prevX + groupXChange,
-      prevY: this.state.prevY + groupYChange
-    });
+      prevY: this.state.prevY + groupYChange,
+    })
   }
 
-  render () {
-    const emojiConfigs = getEmojiConfigs(Object.values(this.props.emojis));
-    const activeEmojiConfig = emojiConfigs.find(config => config['data-id'] === this.props.activeEmojiId)
+  render() {
+    const emojiConfigs = getEmojiConfigs(Object.values(this.props.emojis))
+    const activeEmojiConfig = emojiConfigs.find(
+      (config) => config['data-id'] === this.props.activeEmojiId
+    )
 
-    const outlineConfig = getOutlineConfig(activeEmojiConfig);
+    const outlineConfig = getOutlineConfig(activeEmojiConfig)
 
     // hide active emoji during drag action
     if (this.state.isDragging) {
-      activeEmojiConfig.opacity = 0;
-      outlineConfig.opacity = 0;
+      activeEmojiConfig.opacity = 0
+      outlineConfig.opacity = 0
     }
 
-    return (
-        <FixedCanvasContainer>
-          <Stage
-            width={theme.canvas.width}
-            height={theme.canvas.height}
-          >
-            <Layer>
-              {/* Canvas */}
-              <Rect
-                x={0}
-                y={0}
-                width={theme.canvas.width}
-                height={theme.canvas.height}
-                fill={this.props.backgroundColor}
-              />
+    console.log('emojiConfigs', emojiConfigs)
 
-              {/* /**
-                * @todo step 3 generating emoji items as images
-                * alternatively, try this approach https://github.com/konvajs/konva/issues/101#issuecomment-149646411
-              */}
-              {/* {this.state.emojiImageObj && <Image
+    return (
+      <FixedCanvasContainer>
+        <Stage width={theme.canvas.width} height={theme.canvas.height}>
+          <Layer>
+            {/* Canvas */}
+            <Rect
+              x={0}
+              y={0}
+              width={theme.canvas.width}
+              height={theme.canvas.height}
+              fill={this.props.backgroundColor}
+            />
+
+            {/* /**
+             * @todo step 3 generating emoji items as images
+             * alternatively, try this approach https://github.com/konvajs/konva/issues/101#issuecomment-149646411
+             */}
+            {/* {this.state.emojiImageObj && <Image
                 draggable
                 rotation={0}
                 width={1000}
@@ -111,49 +108,53 @@ class EmojiCanvas extends Component {
                 image={this.state.emojiImageObj}
               />} */}
 
-              {emojiConfigs.map(config => <Text
+            {emojiConfigs.map((config) => (
+              <Text
                 useCache
-                ref={ref => this.props.emojiRefs[config['data-id']] = ref}
+                ref={(ref) => (this.props.emojiRefs[config['data-id']] = ref)}
                 key={`${config['data-id']}${config.emoji}`}
                 {...config}
-              />)}
+              />
+            ))}
 
-              {/* Draggable layer */}
-              <Group
-                draggable
-                onDragStart={this.onDragStart}
-                onDragEnd={this.onDragEnd}
+            {/* Draggable layer */}
+            <Group
+              draggable
+              onDragStart={this.onDragStart}
+              onDragEnd={this.onDragEnd}
+              x={this.state.prevX}
+              y={this.state.prevX}
+            >
+              <Rect
+                width={theme.canvas.width}
+                height={theme.canvas.height}
                 x={this.state.prevX}
                 y={this.state.prevX}
-              >
-                <Rect
-                  width={theme.canvas.width}
-                  height={theme.canvas.height}
-                  x={this.state.prevX}
-                  y={this.state.prevX}
-                />
-                <Text
-                  {...activeEmojiConfig}
-                  useCache
-                  ref={ref => this.props.emojiRefs[EMOJI_MASK_REF_ID] = ref}
-                  opacity={this.state.isDragging ? 0.25 : 0}
-                />
-                <Text
-                  {...outlineConfig}
-                  ref={ref => this.props.emojiRefs[EMOJI_MASK_OUTLINE_REF_ID] = ref}
-                  filters={[Konva.Filters.RGBA]}
-                  alpha={1}
-                  red={255}
-                  green={76}
-                  blue={127}
-                  opacity={0.5}
-                />
-              </Group>
-            </Layer>
-          </Stage>
-        </FixedCanvasContainer>
+              />
+              <Text
+                {...activeEmojiConfig}
+                useCache
+                ref={(ref) => (this.props.emojiRefs[EMOJI_MASK_REF_ID] = ref)}
+                opacity={this.state.isDragging ? 0.25 : 0}
+              />
+              <Text
+                {...outlineConfig}
+                ref={(ref) =>
+                  (this.props.emojiRefs[EMOJI_MASK_OUTLINE_REF_ID] = ref)
+                }
+                filters={[Konva.Filters.RGBA]}
+                alpha={1}
+                red={255}
+                green={76}
+                blue={127}
+                opacity={0.5}
+              />
+            </Group>
+          </Layer>
+        </Stage>
+      </FixedCanvasContainer>
     )
   }
 }
 
-export default EmojiCanvas;
+export default EmojiCanvas
